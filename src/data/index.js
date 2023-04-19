12b5e50f3ddacce8463eb0c6617d2214e147d6ed
emma-junior <emnzboy31@gmail.com>
2023-04-19 23:59:20 +0100

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,33 @@
+import picOne from "../assets/images/picture-one.png";
+import picTwo from "../assets/images/picture-two.png";
+import picThree from "../assets/images/picture-three.png";
+import picFour from "../assets/images/picture-four.png";
+import picFive from "../assets/images/picture-five.png";
+import picSix from "../assets/images/picture-six.png";
+
+export const data = [
+  {
+    name: "Emmanuel",
+    img: picOne,
+  },
+  {
+    name: "Emmanuel",
+    img: picTwo,
+  },
+  {
+    name: "Emmanuel",
+    img: picThree,
+  },
+  {
+    name: "Emmanuel",
+    img: picFour,
+  },
+  {
+    name: "Emmanuel",
+    img: picFive,
+  },
+  {
+    name: "Emmanuel",
+    img: picSix,
+  },
+];
